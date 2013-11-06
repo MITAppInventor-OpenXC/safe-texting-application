4b7c3ca4fed1247d9f3773d43344688450a9dc56
Phu Nguyen <pnguyen@evernote.com>
2013-11-06 11:38:23 -0500

    Fixed indents

--- safe-texting-application/src/com/example/safe_texting_application/SmsReceiver.java
+++ safe-texting-application/src/com/example/safe_texting_application/SmsReceiver.java
@@ -9,21 +9,21 @@ import android.telephony.SmsMessage;
 
 public class SmsReceiver extends BroadcastReceiver {
 
-    public void onReceive(Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
-        SmsMessage[] msgs = null;
-        
-        if (bundle != null) {
-            Object[] pdus = (Object[]) bundle.get("pdus");
-            msgs = new SmsMessage[pdus.length];
-            for (int i = 0; i < msgs.length; i++) {
-                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-            }
-            for (SmsMessage msg : msgs) {
-            	String strFrom = msg.getDisplayOriginatingAddress();
-            	String strMsg = msg.getDisplayMessageBody();
-            	MainActivity.getInstance().speakWords(strMsg);
-            }
-        }
+	public void onReceive(Context context, Intent intent) {
+		Bundle bundle = intent.getExtras();
+		SmsMessage[] msgs = null;
+
+		if (bundle != null) {
+			Object[] pdus = (Object[]) bundle.get("pdus");
+			msgs = new SmsMessage[pdus.length];
+			for (int i = 0; i < msgs.length; i++) {
+				msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+			}
+			for (SmsMessage msg : msgs) {
+				String strFrom = msg.getDisplayOriginatingAddress();
+				String strMsg = msg.getDisplayMessageBody();
+				MainActivity.getInstance().speakWords(strMsg);
+			}
+		}
     }
 }
